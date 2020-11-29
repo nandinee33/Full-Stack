2b551f042f04786e522827a0be6989d3acc1637f
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-11-29 18:29:06 +0530

    practical-18

--- practicals/practical-18/practical-18.js
+++ practicals/practical-18/practical-18.js
@@ -1,20 +1,22 @@
 // Selectors
 
 const form = document.getElementById('form');
-const username = document.getElementById('username');
-const email = document.getElementById('email');
-const password1 = document.getElementById('password1');
-const password2 = document.getElementById('password2');
-const small = document.querySelector('small');
+const username= document.getElementById('username');
+const email= document.getElementById('email');
+const password1= document.getElementById('password1');
+const password2= document.getElementById('password2');
+const small= document.querySelector('small');
 
 
 
 // Event Listner
 
+//form submission
 // e tells us that which event is call
 form.addEventListener('submit', function(e){
     // stop the default behaviour of submit
     // means data is not refreshing anymore
+    //to stop submission
     e.preventDefault();
     // when data is  submitted we have to check all the inputs
     //s o we make this function to check all the inputs
@@ -28,65 +30,67 @@ form.addEventListener('submit', function(e){
 function checkInput()
 {
     // trim removes the white spaces in the input 
-    const usernameValue = username.value.trim() ;
+    const usernameValue = username.value.trim();
     // console.log(usernameValue)
-    const emailValue = email.value.trim() ;
-    const password1Value = password1.value.trim() ;
-    const password2Value = password2.value.trim() ;
+    
+    const emailValue = email.value.trim();
+    const password1Value = password1.value.trim();
+    const password2Value = password2.value.trim();
 
 
+   
+    // -----------------Username-----------------
     // if value in the username is empty
-    if(usernameValue === '')
-    {
+    if(usernameValue===''){
         // then showError function is called , which tells that error comes from username with this error message
-        showError(username, "Username can not be blank");
+        showError(username, "Username can't be blank");
     }
-    else
+    else if(!isUsernameValid(usernameValue))
     {
+        showError(username, "Username can have small letters and underscores only");
+    }
+    else {
         showSuccess(username);
     }
 
 
-    if(emailValue === '')
+
+    //-----------------email-----------------
+    if(emailValue==='')
     {
-        // then showError function is called , which tells that error comes from username with this error message
-        showError(email, "email can not be blank");
+        showError(email, "Email Id can't be blank");
     }
-    else if(isEmailValid(emailValue))
+    else if(!isEmailValid(emailValue))
     {
-        showError(email,"Email ID is not Valid");
+        showError(email, "Email Id is inavalid");
     }
-    else
-    {
+    else{
         showSuccess(email);
     }
 
 
-    if(password1Value === '')
-    {
-        // then showError function is called , which tells that error comes from username with this error message
-        showError(password1, "password can not be blank");
+    // ---------------password--------------
+    if(password1Value===''){
+        showError(password1, "Password can't be blank");
     }
-    else
-    {
+    else {
         showSuccess(password1);
     }
 
 
-    if(password2Value === '')
-    {
-        // then showError function is called , which tells that error comes from username with this error message
-        showError(password2, "password can not be blank");
+    //--------------conferm password------------
+    if(password2Value===''){
+        showError(password2, "Re-enter password");
     }
     else if(password2Value != password1Value)
     {
-        showError(password2, "passwords not match");
+        showError(password2, "Password not matched!")
     }
-    else
-    {
+    else {
         showSuccess(password2);
     }
 }
+
 
 
 function showError(input, msg)
@@ -96,10 +100,11 @@ function showError(input, msg)
     // add a new class of formControl
     formControl.className = 'form-control error';
     // formControl ke andar hme small ko select krna h 
-    const  small = formControl.querySelector('small');
+    const small= formControl.querySelector('small');
     // set msg in small which is come from error
-    small.innerHTML = msg;
+    small.innerHTML= msg;
 }
+
 
 
 function showSuccess(input)
@@ -111,8 +116,15 @@ function showSuccess(input)
 }
 
 
+
 function isEmailValid(email)
 {
     // email validation regex
-    return /^([a-zA-Z0-9_\.\-\+]+)@([a-zA-Z0-9_\.\-\+]+)\.([a-zA-Z]){2,3})$/.test(email);
+    return /^([a-zA-Z0-9_\.\-\+]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email);
+}
+
+function isUsernameValid(username)
+{
+    // username validation regex
+    return /^[a-z_]+$/.test(username);
 }
